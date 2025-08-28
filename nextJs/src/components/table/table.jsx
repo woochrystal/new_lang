8172e0e535e4774562ef357363225727f44de79f
Woo Sujeong <woochryfront@gmail.com>
2025-08-28 18:02:35 +0900

    테이블 데이터 추가

--- nextJs/src/components/table/table.jsx
+++ nextJs/src/components/table/table.jsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from 'react'
 import styles from './table.module.scss'
 export default function TableLayout(){
-
+    const theadList = ['No.', '상신 일시', '기안자', '기안 부서', '휴가 종류', '휴가 기간', '휴가 일수', '진행 상태']
+    const tbodyList = [ '12', '2025-08-25 14:14:14', '홍길동', '전략기획부', '연차', '2025-08-25 14:14:14', '2', '승인']
 
 
 
