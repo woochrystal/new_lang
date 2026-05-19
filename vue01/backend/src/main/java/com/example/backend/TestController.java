50f2dd2660297863bf8431de21c0a90cf5ee72cb
Woo Sujeong <woochryfront@gmail.com>
2026-05-19 17:58:54 +0900

    feat: java json 받아오기

--- vue01/backend/src/main/java/com/example/backend/TestController.java
+++ vue01/backend/src/main/java/com/example/backend/TestController.java
@@ -4,14 +4,22 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.HashMap;
+
 //localhost:5173(Vue)에서 오는 요청 허용
 @CrossOrigin(origins = "http://localhost:5173")
 
 @RestController public class TestController {
 
     @GetMapping("/api/test")
-    public String test(){
-        return "백엔드 연결 성공";
+    public Map<String, Object> test(){
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        data.put("name", "홍길동");
+        data.put("age", 29);
+
+        return data;
     }
 
 }
